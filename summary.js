2e5981819a2760f9a7da9732b69e27ad653c76fa
rahathossain3 <rahathossain210@gmail.com>
2022-03-01 17:12:59 +0600

    summary

---
+++ summary.js
@@ -0,0 +1,17 @@
+//== & ===
+// type of variable
+
+
+// primitive
+
+// non primitive {object}
+
+// Array.isArray(nums);
+
+function triple(x, y, z) {
+
+}
+
+// console.log(typeof triple);
+// console.log(triple.length);
+console.log(typeof null);
